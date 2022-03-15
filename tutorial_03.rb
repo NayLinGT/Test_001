0d60a7d896af012aecad6833f6349a3a48b07e31
NayLinGT <naylin@seattleconsultingmyanmar.com>
2022-03-15 15:17:49 +0630

    df

--- tutorial_03.rb
+++ tutorial_03.rb
@@ -1,1 +1,4 @@
 print "hello tutorial 03."
+
+What is webpack?
+The goal of webpack, or any front-end build system, is to allow you to write your front-end code in a way that is convenient for developers and then package that code in a way that is convenient for browsers. With webpack, you can manage JavaScript, CSS, and static assets like images or fonts. Webpack will allow you to write your code, reference other code in your application, transform your code, and combine your code into easily downloadable packs.
